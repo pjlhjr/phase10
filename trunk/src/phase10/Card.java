d80a81905417b2c0b6cb0d14a0bebb253f7c4f8d
forbes.evan <forbes.evan@e05f8a96-d257-8df0-a514-86e075d47d78>
2012-10-25 17:47:56 +0000

    Test Commit

--- trunk/src/phase10/Card.java
+++ trunk/src/phase10/Card.java
@@ -11,6 +11,8 @@ import java.io.Serializable;
  * This class contains all of the information for each playing card for the
  * Phase 10 game.
  * 
+ * CHANGED CHANGED CHANGED
+ * 
  * @author Evan Forbes
  * 
  */
