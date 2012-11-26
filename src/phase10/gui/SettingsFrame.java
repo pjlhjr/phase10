48822bacede5e9fdf6e4685e46d115b355423bf9
matthew.hruz@gmail.com <matthew.hruz@gmail.com@e05f8a96-d257-8df0-a514-86e075d47d78>
2012-11-26 20:28:11 +0000

    The world comes crashing down

--- src/phase10/gui/SettingsFrame.java
+++ src/phase10/gui/SettingsFrame.java
@@ -27,6 +27,10 @@ import phase10.GameManager;
 import phase10.Player;
 import java.awt.event.MouseAdapter;
 import java.awt.Toolkit;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 
 public class SettingsFrame extends JFrame {
 
@@ -36,9 +40,10 @@ public class SettingsFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField nameField;
-	private JTextField opponentField_1;
-	private JTextField opponentField_2;
-	private JTextField opponentField_3;
+	protected JTextField opponentField_1;
+	protected JTextField opponentField_2;
+	protected JTextField opponentField_3;
+	private JButton btnRemovePlayer;
 
 
 	/**
@@ -49,7 +54,6 @@ public class SettingsFrame extends JFrame {
 			public void run() {
 				try {
 					SettingsFrame frame = new SettingsFrame(new GuiManager(new GameManager()));
-					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -65,7 +69,7 @@ public class SettingsFrame extends JFrame {
 	public SettingsFrame(GuiManager gManage) {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(SettingsFrame.class.getResource("/images/GameIcon.png")));
 		Language lang = gManage.getGameLang();
-		
+
 		setResizable(false);
 		setTitle(lang.getEntry("SETTINGS_FRAME_TITLE"));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -89,14 +93,16 @@ public class SettingsFrame extends JFrame {
 		contentPane.add(languageLabel);
 
 		JComboBox comboBox = new JComboBox();
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				System.out.println("Item state has changed!");
+			}
+		});
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"English", "Español", "Français", "Deutsch"}));
 		comboBox.setBounds(438, 58, 92, 20);
 		contentPane.add(comboBox);
 
-		JButton btnAddPlayer = new JButton(lang.getEntry("ADD_PLAYER"));
-		btnAddPlayer.setBounds(143, 291, 109, 23);
-		contentPane.add(btnAddPlayer);
-		
+
 		final JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(438, 129, 79, 20);
 		contentPane.add(comboBox_1);
@@ -105,6 +111,7 @@ public class SettingsFrame extends JFrame {
 		comboBox_1.addItem(lang.getEntry("HARD"));
 
 		final JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setVisible(false);
 		comboBox_2.setBounds(438, 186, 79, 20);
 		contentPane.add(comboBox_2);
 		comboBox_2.addItem(lang.getEntry("EASY"));
@@ -112,40 +119,45 @@ public class SettingsFrame extends JFrame {
 		comboBox_2.addItem(lang.getEntry("HARD"));
 
 		final JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setVisible(false);
 		comboBox_3.setBounds(438, 244, 79, 20);
 		contentPane.add(comboBox_3);
 		comboBox_3.addItem(lang.getEntry("EASY"));
 		comboBox_3.addItem(lang.getEntry("MEDIUM"));
 		comboBox_3.addItem(lang.getEntry("HARD"));
 
+		JSeparator separator = new JSeparator();
+		separator.setBounds(27, 104, 503, 2);
+		contentPane.add(separator);
+
 		opponentField_1 = new JTextField();
 		opponentField_1.setBounds(143, 129, 128, 20);
 		contentPane.add(opponentField_1);
 		opponentField_1.setColumns(10);
 
-		JSeparator separator = new JSeparator();
-		separator.setBounds(27, 104, 503, 2);
-		contentPane.add(separator);
-
 		opponentField_2 = new JTextField();
+		opponentField_2.setVisible(false);
 		opponentField_2.setBounds(143, 186, 128, 20);
 		contentPane.add(opponentField_2);
 		opponentField_2.setColumns(10);
 
 		opponentField_3 = new JTextField();
+		opponentField_3.setVisible(false);
 		opponentField_3.setBounds(143, 244, 128, 20);
 		contentPane.add(opponentField_3);
 		opponentField_3.setColumns(10);
 
-		JLabel lblOpponent = new JLabel(lang.getEntry("OPPONENT") + ": 1");
+		final JLabel lblOpponent = new JLabel(lang.getEntry("OPPONENT") + " 1:");
 		lblOpponent.setBounds(27, 132, 92, 14);
 		contentPane.add(lblOpponent);
 
-		JLabel lblOpponent_1 = new JLabel(lang.getEntry("OPPONENT") + ": 2");
+		final JLabel lblOpponent_1 = new JLabel(lang.getEntry("OPPONENT") + " 2:");
+		lblOpponent_1.setVisible(false);
 		lblOpponent_1.setBounds(27, 185, 92, 14);
 		contentPane.add(lblOpponent_1);
 
-		JLabel lblOpponent_2 = new JLabel(lang.getEntry("OPPONENT") + ": 3");
+		final JLabel lblOpponent_2 = new JLabel(lang.getEntry("OPPONENT") + " 3:");
+		lblOpponent_2.setVisible(false);
 		lblOpponent_2.setBounds(27, 247, 92, 14);
 		contentPane.add(lblOpponent_2);
 
@@ -159,84 +171,88 @@ public class SettingsFrame extends JFrame {
 		/*
 		 * BEGIN RADIO BUTTONS
 		 */
-		
+
 		//opponent 1 set
 		ButtonGroup opp1Group = new ButtonGroup();
-		JRadioButton radioButton = new JRadioButton(lang.getEntry("HUMAN"));
-		radioButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				invisibleSetter(comboBox_2 ,false);
-			}
-		});
-		radioButton.setBounds(295, 178, 109, 23);
-		opp1Group.add(radioButton);
-		contentPane.add(radioButton);
-
-		JRadioButton radioButton_1 = new JRadioButton(lang.getEntry("COMPUTER"));
-		radioButton_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				invisibleSetter(comboBox_2, true);
-			}
-		});
-		radioButton_1.setSelected(true);
-		radioButton_1.setBounds(295, 198, 109, 23);
-		opp1Group.add(radioButton_1);
-		contentPane.add(radioButton_1);
-
-		//opponent 2 set
-		ButtonGroup opp2Group = new ButtonGroup();
-		JRadioButton radioButton_2 = new JRadioButton(lang.getEntry("HUMAN"));
-		radioButton_2.addMouseListener(new MouseAdapter() {
+		final JRadioButton humanRadio1 = new JRadioButton(lang.getEntry("HUMAN"));
+		humanRadio1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				invisibleSetter(comboBox_1, false);
 			}
 		});
-		//		private final Action action = new HumanRadioAction(null, null);
-		//		radioButton_2.setAction(action);
-		radioButton_2.setBounds(295, 113, 109, 23);
-		opp2Group.add(radioButton_2);
-		contentPane.add(radioButton_2);
+		humanRadio1.setBounds(295, 113, 109, 23);
+		//radioButton_2.setVisible(false);
+		opp1Group.add(humanRadio1);
+		contentPane.add(humanRadio1);
 
-		JRadioButton radioButton_3 = new JRadioButton(lang.getEntry("COMPUTER"));
-		radioButton_3.addMouseListener(new MouseAdapter() {
+		final JRadioButton computerRadio1 = new JRadioButton(lang.getEntry("COMPUTER"));
+		computerRadio1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				invisibleSetter(comboBox_1 ,true);
 			}
 		});
-		radioButton_3.setSelected(true);
-		radioButton_3.setBounds(295, 133, 109, 23);
-		opp2Group.add(radioButton_3);
-		contentPane.add(radioButton_3);
+		computerRadio1.setSelected(true);
+		computerRadio1.setBounds(295, 133, 109, 23);
+		opp1Group.add(computerRadio1);
+		contentPane.add(computerRadio1);
+
+		//opponent 2 set
+		ButtonGroup opp2Group = new ButtonGroup();
+		final JRadioButton humanRadio2 = new JRadioButton(lang.getEntry("HUMAN"));
+		humanRadio2.setVisible(false);
+		humanRadio2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				invisibleSetter(comboBox_2 ,false);
+			}
+		});
+		humanRadio2.setBounds(295, 178, 109, 23);
+		opp2Group.add(humanRadio2);
+		contentPane.add(humanRadio2);
+
+		final JRadioButton computerRadio2 = new JRadioButton(lang.getEntry("COMPUTER"));
+		computerRadio2.setVisible(false);
+		computerRadio2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				invisibleSetter(comboBox_2, true);
+			}
+		});
+		computerRadio2.setSelected(true);
+		computerRadio2.setBounds(295, 198, 109, 23);
+		opp2Group.add(computerRadio2);
+		contentPane.add(computerRadio2);
 
 		//opponent 3 set
 		ButtonGroup opp3Group = new ButtonGroup();
-		JRadioButton radioButton_4 = new JRadioButton(lang.getEntry("HUMAN"));
-		radioButton_4.addMouseListener(new MouseAdapter() {
+		final JRadioButton humanRadio3 = new JRadioButton(lang.getEntry("HUMAN"));
+		humanRadio3.setVisible(false);
+		humanRadio3.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				invisibleSetter(comboBox_3, false);
 			}
 		});
-		radioButton_4.setBounds(295, 238, 109, 23);
-		opp3Group.add(radioButton_4);
-		contentPane.add(radioButton_4);
+		humanRadio3.setBounds(295, 238, 109, 23);
+		//radioButton_4.setVisible(false);
+		opp3Group.add(humanRadio3);
+		contentPane.add(humanRadio3);
 
-		JRadioButton radioButton_5 = new JRadioButton(lang.getEntry("COMPUTER"));
-		radioButton_5.addMouseListener(new MouseAdapter() {
+		final JRadioButton computerRadio3 = new JRadioButton(lang.getEntry("COMPUTER"));
+		computerRadio3.setVisible(false);
+		computerRadio3.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				invisibleSetter(comboBox_3, true);
 			}
 		});
-		radioButton_5.setSelected(true);
-		radioButton_5.setBounds(295, 258, 109, 23);
-		opp3Group.add(radioButton_5);
-		contentPane.add(radioButton_5);
-		
+		computerRadio3.setSelected(true);
+		computerRadio3.setBounds(295, 258, 109, 23);
+		opp3Group.add(computerRadio3);
+		contentPane.add(computerRadio3);
+
 		/*
 		 * END RADIO BUTTONS
 		 */
@@ -245,8 +261,64 @@ public class SettingsFrame extends JFrame {
 		JButton beginButton = new JButton(lang.getEntry("BEGIN"));
 		beginButton.setBounds(227, 333, 109, 42);
 		contentPane.add(beginButton);
-		beginButton.addMouseListener(new BeginListener(this, gManage));
 
+		final JButton btnAddPlayer = new JButton(lang.getEntry("ADD_PLAYER"));
+		btnAddPlayer.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(opponentField_2.isVisible() == false) {
+					invisibleSetter(btnRemovePlayer,true);
+					invisibleSetter(opponentField_2,true);
+					invisibleSetter(humanRadio2, true);
+					invisibleSetter(computerRadio2, true);
+					invisibleSetter(lblOpponent_1, true);
+					invisibleSetter(comboBox_2, true);
+				}
+				else if(opponentField_3.isVisible() == false) {
+					invisibleSetter(opponentField_3,true);
+					invisibleSetter(humanRadio3, true);
+					invisibleSetter(computerRadio3, true);
+					invisibleSetter(lblOpponent_2, true);
+					invisibleSetter(comboBox_3, true);
+					invisibleSetter(btnAddPlayer, false);
+				}
+				else {
+					System.out.println("There was an error!");
+				}
+			}
+		});
+		btnAddPlayer.setBounds(41, 292, 109, 23);
+		contentPane.add(btnAddPlayer);
+
+		btnRemovePlayer = new JButton("Remove last opponent");
+		btnRemovePlayer.setVisible(false);
+		btnRemovePlayer.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(opponentField_3.isVisible() == true) {
+					invisibleSetter(opponentField_3,false);
+					invisibleSetter(humanRadio3, false);
+					invisibleSetter(computerRadio3, false);
+					invisibleSetter(lblOpponent_2, false);
+					invisibleSetter(comboBox_3, false);
+					invisibleSetter(btnAddPlayer, true);
+				}
+				else if(opponentField_2.isVisible() == true) {
+					invisibleSetter(opponentField_2, false);
+					invisibleSetter(humanRadio2, false);
+					invisibleSetter(computerRadio2, false);
+					invisibleSetter(lblOpponent_1, false);
+					invisibleSetter(comboBox_2, false);
+					invisibleSetter(btnRemovePlayer, false);
+				}
+				else {
+					System.out.println("An internal error occured while trying to remove players!");
+				}
+			}
+		});
+		btnRemovePlayer.setBounds(160, 292, 141, 23);
+		contentPane.add(btnRemovePlayer);
+		beginButton.addMouseListener(new BeginListener(this, gManage));
 	}
 
 	public String getUserName() {
@@ -283,22 +355,45 @@ public class SettingsFrame extends JFrame {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 
-			if(settingsFrm.getUserName() != "")
-				gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getUserName())); //TODO Null pointer exception here
-			if(settingsFrm.getOpponent_1() != "")
-				gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getOpponent_1())); //TODO Null pointer exception here
-			if(settingsFrm.getOpponent_1() != "")
-				gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getOpponent_2())); //TODO Null pointer exception here
-			if(settingsFrm.getOpponent_1() != "")
-				gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getOpponent_2())); //TODO Null pointer exception here
-			
-			/*
-			 * test declarations
-			 */
-//			System.out.println(settingsFrm.getUserName());
-//			System.out.println(settingsFrm.getOpponent_1());
-//			System.out.println(settingsFrm.getOpponent_2());
-//			System.out.println(settingsFrm.getOpponent_3());
+			if(settingsFrm.getUserName().isEmpty()) {
+				MessageFrame errorFrame = new MessageFrame("Please put in a name for yourself", "Phase 10");
+				errorFrame.setVisible(true);
+				return;
+			}
+			else {
+				gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getUserName(), gManage.mainManager.getGame()));
+			}
+				if(settingsFrm.getOpponent_1().isEmpty()) {
+					MessageFrame errorFrame = new MessageFrame("Please put in a name for player 1", "Phase 10");
+					errorFrame.setVisible(true);
+					return;
+				}
+				else {
+					gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getOpponent_1(), gManage.mainManager.getGame()));
+				}
+			if(settingsFrm.opponentField_2.isVisible()) {
+				if(settingsFrm.getOpponent_2().isEmpty()) {
+					MessageFrame errorFrame = new MessageFrame("Please put in a name for player 2", "Phase 10");
+					errorFrame.setVisible(true);
+					return;
+				}
+				else {
+					gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getOpponent_2(), gManage.mainManager.getGame()));
+				}
+			}
+			if(settingsFrm.opponentField_3.isVisible()) {
+				if(settingsFrm.getOpponent_3().isEmpty()) {
+					MessageFrame errorFrame = new MessageFrame("Please put in a name for player 3", "Phase 10");
+					errorFrame.setVisible(true);
+					return;
+				}
+				else {
+					gManage.mainManager.getGame().addPlayer(new Player(settingsFrm.getOpponent_3(), gManage.mainManager.getGame()));
+				}
+			}
+
+			gManage.mainManager.getGame().startGame();
+			gManage.displayGameFrame(); //displays the next window: the game window
 
 			settingsFrm.dispose();
 		}
