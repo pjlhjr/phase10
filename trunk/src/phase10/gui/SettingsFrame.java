b27416d87c60fcab43a83deac4e4e119a97eadd0
matthew.hruz@gmail.com <matthew.hruz@gmail.com@e05f8a96-d257-8df0-a514-86e075d47d78>
2012-12-03 16:40:14 +0000

    oh, and you need these updates for a fully functioning game.

--- trunk/src/phase10/gui/SettingsFrame.java
+++ trunk/src/phase10/gui/SettingsFrame.java
@@ -430,6 +430,7 @@ public class SettingsFrame extends JFrame {
 			}
 
 			gManage.mainManager.getGame().startGame();
+			gManage.mainManager.getGame().getRound().startRound();
 			gManage.initGameWindow();
 			gManage.displayGameFrame(); //displays the next window: the game window
 			
