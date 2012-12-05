05c76951a92ff2c616f9ded397c52cc260fd9f08
forbes.evan <forbes.evan@e05f8a96-d257-8df0-a514-86e075d47d78>
2012-12-05 01:30:29 +0000

    Made addPhaseGroups method clearer. Added config for information about each phase

--- trunk/src/phase10/util/Configuration.java
+++ trunk/src/phase10/util/Configuration.java
@@ -8,13 +8,15 @@ public class Configuration {
 	public static final int NUM_WILDS = 8;
 	public static final int NUM_SKIPS = 4;
 	public static final int NUM_CARDS_TO_DEAL = 10;
+
 	public static final int WILD_VALUE = 13;
 	public static final int SKIP_VALUE = 14;
+
 	public static final int SET_PHASE = 0;
 	public static final int RUN_PHASE = 1;
 	public static final int COLOR_PHASE = 2;
 
-	//Set these to false when not testing
+	// Set these to false when not testing
 	public static final boolean PRINT_LOG = true;
 	public static final boolean SAVE_LOG = true;
 	public static final String LOG_FILE = "log.txt";
@@ -25,8 +27,31 @@ public class Configuration {
 	public static final String[] COLOR_NAMES = {"Red", "Blue", "Green",
 			"Yellow"};
 
-	//The number of phase groups required for each phase
-	private static int[] numPhasesExpected = {2, 2, 2, 1, 1, 1, 2, 1, 2, 2};
+	private static int[][] typePhasesRequired = {
+		{SET_PHASE, SET_PHASE},
+		{SET_PHASE, RUN_PHASE},
+		{SET_PHASE, RUN_PHASE},
+		{RUN_PHASE},
+		{RUN_PHASE},
+		{RUN_PHASE},
+		{SET_PHASE, SET_PHASE},
+		{COLOR_PHASE},
+		{SET_PHASE, SET_PHASE},
+		{SET_PHASE, SET_PHASE}
+	};
+
+	private static int[][] minLengthOfPhasesRequired = {
+		{3, 3},
+		{3, 4},
+		{4, 4},
+		{7},
+		{8},
+		{9},
+		{4, 4},
+		{7},
+		{5, 2},
+		{5, 3}
+	};
 
 	/**
 	 * Gets the number of phase groups required given the phase
@@ -35,7 +60,33 @@ public class Configuration {
 	 *            the phase
 	 * @return the number of phase groups required
 	 */
-	public static int getNumberOfPhaseGroupsRequired(int phase) {
-		return numPhasesExpected[phase - 1];
+	public static int getNumberRequired(int phase) {
+		return typePhasesRequired[phase - 1].length;
+	}
+
+	/**
+	 * Gets the types of phases required for the given phase
+	 * 
+	 * @param phase
+	 *            the phase
+	 * @param groupIndex
+	 *            the phasegroup index
+	 * @return the type of phases required
+	 */
+	public static int getTypeRequired(int phase, int groupIndex) {
+		return typePhasesRequired[phase - 1][groupIndex];
+	}
+
+	/**
+	 * Get the lengths required for each phase group for the given phase
+	 * 
+	 * @param phase
+	 *            the phase
+	 * @param groupIndex
+	 *            the phase group index
+	 * @return the required lengths for the each phase group
+	 */
+	public static int getLengthRequired(int phase, int groupIndex) {
+		return minLengthOfPhasesRequired[phase - 1][groupIndex];
 	}
 }
