af70ee493a0a56bc7b13ae3b32a77c2b4c4e2f70
pjlhjr@gmail.com <pjlhjr@gmail.com@e05f8a96-d257-8df0-a514-86e075d47d78>
2012-12-07 22:08:02 +0000

    This should be my final commit. Finished commenting/code clean up. Added more difficulty variance. Added easter egg. Thanks guys. Great work.

--- trunk/src/phase10/ai/Groups.java
+++ trunk/src/phase10/ai/Groups.java
@@ -13,14 +13,14 @@ import phase10.card.CardValueComparator;
 import phase10.util.Configuration;
 
 class Groups{
-	private ArrayList<Card> single;
-	private ArrayList<Card>	cardConflict;
-	private Card[] cards;
-	private ArrayList<PhaseGroup> 	complete;
-	private ArrayList<PhaseGroup>	partial;
-	private ArrayList<ArrayList<PhaseGroup>>	conflictingGroups;
-	private AIPlayer player;
-	
+	private ArrayList<Card> single; // cards that are not in any PhaseGroups
+	private ArrayList<Card>	cardConflict; // a card that is contained by multiple phaseGroups
+	private Card[] cards; // the cards in the hand
+	private ArrayList<PhaseGroup> 	complete; // a phasegroup that is validated
+	private ArrayList<PhaseGroup>	partial; // a phasegroup that is not validated
+	private ArrayList<ArrayList<PhaseGroup>>	conflictingGroups; // PhaseGroups that contain cards that are also in another PhaseGroup
+	private AIPlayer player; // The AIPlayer whose hand this is
+
 	/**
 	 * @param p the AIPlayer holding the hand
 	 */
@@ -43,9 +43,11 @@ class Groups{
 		for(int index = 0; index < h.getNumberOfCards(); index++){
 			cards[index] = h.getCard(index);
 		}
-		group();
+
+		if(!player.hasLaidDownPhase())
+			group();
 	}
-	
+
 	/**
 	 * @param p the AIPlayer holding the hand
 	 * @param h the hand the player is hold
@@ -58,15 +60,16 @@ class Groups{
 			cards[index] = h.getCard(index);
 		}
 		cards[cards.length - 1] = c;
-		
+
 		if(player.colorPhase())
 			Arrays.sort(cards, new CardColorComparator());
 		else
 			Arrays.sort(cards, new CardValueComparator());
-		
-		group();
+
+		if(!player.hasLaidDownPhase())
+			group();
 	}
-	
+
 	/** 
 	 * @param card the that you want to see if it's in a complete group
 	 * @return true, if the card is in a complete group. false, if the card is not found
@@ -80,34 +83,34 @@ class Groups{
 		}
 		return false;
 	}
-	
+
 	/**
 	 * @return returns the point value of the hand
 	 */
 	public int getPointValue(){
 		boolean countIt;
 		int val = 0;
-		
+
 		for(Card c: cards){
 			countIt = true;
-			
+
 			counted:
-			for(PhaseGroup g: complete){
-				for(int groupIndex = 0; groupIndex < g.getNumberOfCards(); groupIndex++){
-					if(c == g.getCard(groupIndex)){ // if the card is found in a complete PhaseGroup
-						countIt = false; // it doesn't count to the total point value of the hand
-						break counted; // stop looking
+				for(PhaseGroup g: complete){
+					for(int groupIndex = 0; groupIndex < g.getNumberOfCards(); groupIndex++){
+						if(c == g.getCard(groupIndex)){ // if the card is found in a complete PhaseGroup
+							countIt = false; // it doesn't count to the total point value of the hand
+							break counted; // stop looking
+						}
 					}
 				}
-			}
-			
+
 			if(countIt){
 				val += c.getPointValue();
 			}
 		}
 		return val;
 	}
-	
+
 	/**
 	 * This method puts the cards into complete, partial, and single groups.
 	 */
@@ -120,10 +123,9 @@ class Groups{
 		connectRuns();
 		validateCompleteGroups();
 	}
+
 	
-	/* figure out some way, break this problem down,  
-	nesseccary? no runs less than 4 means must be a partial group present
-	
+	/*
 	private void connectedGroup(){
 		for(PhaseGroup g: complete){
 			if(g.getType() == Configuration.RUN_PHASE){
@@ -160,30 +162,33 @@ class Groups{
 		}
 		for(Card c: single){
 			if(c instanceof WildCard){
-				
+
 			}else{
-				
+
 			}
 		}
 	}*/
-	 
-	private void connectRuns() { //TODO connect 3 runs/single cards
+
+	/**
+	 * compares all partial runs to one another to see if they can be linked
+	 */
+	private void connectRuns() { 
 		if(player.lengthOfRunNeeded() > 0){
 			for(PhaseGroup p1: partial){
 				if(p1.getType() == Configuration.RUN_PHASE){
 					for(PhaseGroup p2: partial){
 						if(p2.getType() == Configuration.RUN_PHASE && p1 != p2){
-							PhaseGroup temp = new PhaseGroup(player.getGame());
+							PhaseGroup temp = new PhaseGroup(player.getGame()); // make a new PhaseGroup, with the two combined
 							for(int x = 0; x < p1.getNumberOfCards(); x++)
 								temp.addCard(p1.getCard(x));
 							for(int x = 0; x < p2.getNumberOfCards(); x++)
 								temp.addCard(p2.getCard(x));
-							if(PhaseGroup.validate(temp, Configuration.RUN_PHASE, player.lengthOfRunNeeded())){
-								partial.remove(p1);
+							if(PhaseGroup.validate(temp, Configuration.RUN_PHASE, player.lengthOfRunNeeded())){ // if the new group is valid
+								partial.remove(p1);// get rid of the old 2
 								partial.remove(p2);
 								temp.setType(Configuration.RUN_PHASE);
-								complete.add(temp);
-								connectRuns();
+								complete.add(temp); // make it a complete group
+								connectRuns(); // keep checking
 								return;
 							}	
 						}
@@ -193,20 +198,22 @@ class Groups{
 		}
 	}
 
-	//TODO promote partials to complete on last sets, if you delete one with a wild
+	/**
+	 * This method makes sure thier are the right number and types of complete groups
+	 */
 	private void validateCompleteGroups(){ 
 		if(!player.colorPhase()){
 			ArrayList<PhaseGroup> completeRuns = new ArrayList<PhaseGroup>();
 			ArrayList<PhaseGroup> completeSets = new ArrayList<PhaseGroup>();
-	
+
 			for(PhaseGroup c: complete){
 				if(c.getType() == Configuration.RUN_PHASE)
 					completeRuns.add(c);
 				else
 					completeSets.add(c);
 			}
-			
-			if(completeRuns.size() > 1){ 
+
+			if(completeRuns.size() > 1){ // If we need to get rid of a run
 				double 	highestValue = Double.MAX_VALUE * -1,
 						tempValue = 0;
 				PhaseGroup highestGroup = null;
@@ -215,7 +222,7 @@ class Groups{
 					for(int y = 0; y < completeRuns.get(x).getNumberOfCards(); y++){
 						tempValue += cardValue(completeRuns.get(x).getCard(y));
 					} 
-					
+
 					// subtract a value off for any conflicts, because it will have somewhere else to go
 					for(ArrayList<PhaseGroup> c: conflictingGroups){
 						for(int y = 0; y < c.size(); y++){
@@ -230,8 +237,8 @@ class Groups{
 							}
 						}
 					}
-					
-					if(tempValue > highestValue){
+
+					if(tempValue > highestValue){ // remove the group that had the high value from complete
 						highestValue = tempValue;
 						if(highestGroup != null)
 							complete.remove(highestGroup);
@@ -241,7 +248,7 @@ class Groups{
 					}
 				}
 			}
-			
+
 			if(completeSets.size() > 0 && completeSets.size() != player.setsNeeded().length){//if too few, look to at partial on phases 9 & 10
 				while(completeSets.size() > player.setsNeeded().length){
 					int lowestSetNum = Configuration.WILD_VALUE;
@@ -255,8 +262,8 @@ class Groups{
 					complete.remove(lowestGroup);
 					completeSets.remove(lowestGroup);
 				}
-				
-				if(player.setsNeeded().length > 1 && completeSets.size() < player.setsNeeded().length){
+
+				if(player.setsNeeded().length > 1 && completeSets.size() < player.setsNeeded().length){ // promote valid partials on phase 9 & 10
 					for(PhaseGroup p: partial){
 						if(PhaseGroup.validate(p, Configuration.SET_PHASE, player.setsNeeded()[1])){
 							complete.add(p);
@@ -268,7 +275,7 @@ class Groups{
 			}
 		}
 	}
-	
+
 	/**
 	 * @param c the card that you want the point value of
 	 * @return the point value of the card + a "tie breaker" value, 1/100th of the face value
@@ -276,7 +283,7 @@ class Groups{
 	private double cardValue(Card c){
 		return c.getPointValue() + (c.getValue()/100.0); 
 	}
-	
+
 	/**
 	 * This method finds conflicts where cards are in two different phase groups, 
 	 * and adds them to conflicting (which is deal with in resolveConflicts).
@@ -288,7 +295,7 @@ class Groups{
 		for(int num = 0; num < cards.length; num++){ 
 			cardIsUsed.add(new ArrayList<PhaseGroup>());
 		}
-		
+
 		// If the card is used in a PhaseGroup, 
 		// add the PhaseGroup it is in to a slot in cardIsUsed at the same index of the card in cards.
 		for(PhaseGroup g: complete){ 
@@ -308,12 +315,12 @@ class Groups{
 				int cardIndex = 0;
 				for(;cardIndex < cards.length; cardIndex++){
 					if(tempCard == cards[cardIndex]){
-						cardIsUsed.get(cardIndex).add(g);//break;
+						cardIsUsed.get(cardIndex).add(g);
 					}
 				}
 			}
 		}
-		
+
 		// if the card is in more than one PhaseGroup,
 		// add the card to cardConflict and the PhaseGroups that hold the card to conflictingGroups
 		for(int x = 0; x < cardIsUsed.size(); x++){
@@ -329,7 +336,7 @@ class Groups{
 			}
 		}
 	}
-	
+
 	/**
 	 * This method creates every possible complete and partial group, according to what turn it is on.
 	 * A partial phaseGroup does not meet the length requirements of the phase, while a complete one does
@@ -388,8 +395,7 @@ class Groups{
 			}
 		}
 	}
-	
-	//TODO set wild value, does it work?? Can the single card of the same value be used instead?
+
 	/**
 	 * makes sure every card is only in one PhaseGroup.
 	 */
@@ -399,38 +405,37 @@ class Groups{
 			int maxPointValue = -1;
 			PhaseGroup maxPhaseGroup = null;
 			int tempPointValue;
-			
+
 			for(Iterator<PhaseGroup> iterator = conflictingGroups.get(0).iterator(); iterator.hasNext();){ // for each conflict:
 				PhaseGroup p = iterator.next();
 				p.removeCard(cardConflict.get(0));
-				
+
 				tempPointValue = 0; //point value if the card is removed from the hand
 				if(!validGroup(p)){ // if it not still a complete group after removing the card
 					for(int x = 0; x < p.getNumberOfCards(); x++){
 						tempPointValue += p.getCard(x).getPointValue(); // get the point value of all the other cards
 					}
-				}//TODO continue if false?
+				}
 				if(tempPointValue > maxPointValue){ // keep track of the phase group that would have the highest point value without the card 
 					maxPointValue = tempPointValue;
 					maxPhaseGroup = p;
 				}
-			}//TODO add to each if all were maxPhaseGroups
+			}
 			if(maxPhaseGroup != null) // the group that would be the most costly after removing the card, gets to keep the card 
 				maxPhaseGroup.addCard(cardConflict.get(0));
-			
+
 			for(PhaseGroup g: conflictingGroups.get(0)){ // make sure no partial groups are left in complete groups
 				if(!validGroup(g) && !partial.contains(g)){
 					complete.remove(g);
 					partial.add(g);
 				}
 			}
-			
+
 			cardConflict.remove(0); // move on to the next one
 			conflictingGroups.remove(0);
 		}
 	}
-	
-	// TODO make this right make sure there is not another phase that is already the length needed
+
 	/**
 	 * @param p The phase group that will be checked
 	 * @return True if it is a complete phaseGroup in this player's phase. False if not.
@@ -440,38 +445,37 @@ class Groups{
 				(p.getType() == Configuration.RUN_PHASE && PhaseGroup.validate(p, p.getType(), player.lengthOfRunNeeded())) ||
 				(p.getType() == Configuration.COLOR_PHASE && PhaseGroup.validate(p, p.getType(), 7)));
 	}
-	
-	// TODO single if there are more than one wild. connect runs with wilds
+
 	/**
 	 * This method adds wilds to every possible phase group.
 	 * The conflicts created are later resolved.
 	 */
 	private void dealWithWilds(){
 		wildLoop:
-		for(Card w: cards){
-			if(w.getValue() != Configuration.WILD_VALUE){
-				continue wildLoop;
-			}
-			for(PhaseGroup c: complete){
-				c.addCard(w);
-			}
-			for(int x = 0; x < partial.size(); x++){
-				PhaseGroup p = partial.get(x);
-				p.addCard(w);
-				if(validGroup(p)){
-					complete.add(p);
-					partial.remove(p);
-					x--;
+			for(Card w: cards){
+				if(w.getValue() != Configuration.WILD_VALUE){
+					continue wildLoop;
+				}
+				for(PhaseGroup c: complete){
+					c.addCard(w);
+				}
+				for(int x = 0; x < partial.size(); x++){
+					PhaseGroup p = partial.get(x);
+					p.addCard(w);
+					if(validGroup(p)){
+						complete.add(p);
+						partial.remove(p);
+						x--;
+					}
 				}
 			}
-		}
 	}
-	
+
 	/**
 	 * @return the complete phase groups, empty array size 1 if no complete groups
 	 */
 	public PhaseGroup[] getCompletePhaseGroups(){
-		if(complete.size() > 0){ // transform from ArrayList to an array (toArray does work with generics)
+		if(complete.size() > 0){ // transform from ArrayList to an array (toArray does not work with generics)
 			PhaseGroup[] temp = new PhaseGroup[complete.size()];
 			int x = 0;
 			for(PhaseGroup c: complete)
@@ -481,62 +485,62 @@ class Groups{
 			return null;
 		}
 	}
-	
+
 	public Card[] recommendDiscard(){ // look at conflicts
 		double[] discardValue = new double[cards.length];
-		
+
 		bigLoop: // this loop assigns a "discard value" to each card, the higher the value more likely it will be discarded
-		for(int x = 0; x < cards.length; x++){
-			if(cards[x].getValue() == Configuration.WILD_VALUE){ // don't discard wilds
-				discardValue[x] = Double.MAX_VALUE * -1;
-				continue bigLoop;
-			}else if(cards[x].getValue() == Configuration.SKIP_VALUE){ // discard skips
-				discardValue[x] = Double.MAX_VALUE;
-				continue bigLoop;
-			}else{ 
-				discardValue[x] = cardValue(cards[x]);
-			}
-			
-			// a hard player knows that on the 
-			// TODO look for duplicates
-			if(player.getDifficulty() > 70 && player.lengthOfRunNeeded() > 4){
-				if((cards[x].getValue() == 6 || cards[x].getValue() == 7)||
-				((cards[x].getValue() == 5 || cards[x].getValue() == 8) && player.lengthOfRunNeeded() >= 8) ||
-				((cards[x].getValue() == 4 || cards[x].getValue() == 9) && player.lengthOfRunNeeded() == 9)){
-					discardValue[x] -= cards[x].getPointValue();
-				}	
-			}			
-			
-			for(Card s: single){  // If the card is not in a group that's all you do for that card
-				if(cards[x] == s){
+			for(int x = 0; x < cards.length; x++){
+				if(cards[x].getValue() == Configuration.WILD_VALUE){ // don't discard wilds
+					discardValue[x] = Double.MAX_VALUE * -1;
 					continue bigLoop;
+				}else if(cards[x].getValue() == Configuration.SKIP_VALUE){ // discard skips
+					discardValue[x] = Double.MAX_VALUE;
+					continue bigLoop;
+				}else{ 
+					discardValue[x] = cardValue(cards[x]);
 				}
-			}
-			
-			for(PhaseGroup c: complete){ // TODO what about excess groups
-				for(int completeIndex = 0; completeIndex < c.getNumberOfCards(); completeIndex++){
-					if(c.getCard(completeIndex) == cards[x]){
-						for(int restOfComplete = 0; restOfComplete < c.getNumberOfCards(); restOfComplete++){
-							discardValue[x] -= c.getCard(restOfComplete).getPointValue();
-						}
-						discardValue[x] *= 5;
+
+				// a hard player knows that on these cards will always need to be picked up if this is the case
+				if(player.getDifficulty() > 70 && player.lengthOfRunNeeded() > 4){
+					if((cards[x].getValue() == 6 || cards[x].getValue() == 7)||
+							((cards[x].getValue() == 5 || cards[x].getValue() == 8) && player.lengthOfRunNeeded() >= 8) ||
+							((cards[x].getValue() == 4 || cards[x].getValue() == 9) && player.lengthOfRunNeeded() == 9)){
+						if(x != 0 && cards[x-1].getValue() != cards[x].getValue()) // check for duplicates
+							discardValue[x] -= cards[x].getPointValue();
+					}	
+				}			
+
+				for(Card s: single){  // If the card is not in a group that's all you do for that card
+					if(cards[x] == s){
 						continue bigLoop;
 					}
 				}
-			}
-			
-			for(PhaseGroup p: partial){ // rethink this, cards in complete groups should be ranked ahead of complete groups
-				for(int partialIndex = 0; partialIndex < p.getNumberOfCards(); partialIndex++){
-					if(p.getCard(partialIndex) == cards[x]){
-						for(int restOfPartial = 0; restOfPartial < p.getNumberOfCards(); restOfPartial++){
-							discardValue[x] -= Configuration.WILD_VALUE - p.getCard(restOfPartial).getPointValue();
+
+				for(PhaseGroup c: complete){ 
+					for(int completeIndex = 0; completeIndex < c.getNumberOfCards(); completeIndex++){
+						if(c.getCard(completeIndex) == cards[x]){
+							for(int restOfComplete = 0; restOfComplete < c.getNumberOfCards(); restOfComplete++){
+								discardValue[x] -= c.getCard(restOfComplete).getPointValue();
+							}
+							discardValue[x] *= 5;
+							continue bigLoop;
 						}
-						continue bigLoop;
+					}
+				}
+
+				for(PhaseGroup p: partial){ 
+					for(int partialIndex = 0; partialIndex < p.getNumberOfCards(); partialIndex++){
+						if(p.getCard(partialIndex) == cards[x]){
+							for(int restOfPartial = 0; restOfPartial < p.getNumberOfCards(); restOfPartial++){
+								discardValue[x] -= Configuration.WILD_VALUE - p.getCard(restOfPartial).getPointValue();
+							}
+							continue bigLoop;
+						}
 					}
 				}
 			}
-		}
-		
+
 		//insertion sort, sorting sortedResults according to the discardValue
 		Card[] sortedResults = Arrays.copyOf(cards, cards.length);
 		for(int x = 0; x < discardValue.length; x++){
@@ -554,7 +558,7 @@ class Groups{
 			discardValue[x] = tempIndex;
 			sortedResults[x] = tempCard;
 		}
-		
+
 		return sortedResults;
-	} // TODO keep cards that opponents are using in groups
+	} 
 }
