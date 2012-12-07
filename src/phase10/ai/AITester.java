377fdc74f9ccd22859d28f845e74a02b812cead9
pjlhjr@gmail.com <pjlhjr@gmail.com@e05f8a96-d257-8df0-a514-86e075d47d78>
2012-12-07 01:53:49 +0000

    Better laying down/picking up of cards

--- src/phase10/ai/AITester.java
+++ src/phase10/ai/AITester.java
@@ -15,12 +15,13 @@ public class AITester {
 		p1WinPercent = new int[101][101];
 		Phase10 ten;
 		 
-		for(int p1Diff = 0; p1Diff <= 100; p1Diff++){
-			for(int p2Diff = 0; p2Diff <= 100; p2Diff++){
+		for(int p1Diff = 100; p1Diff >= 0; p1Diff--){
+			for(int p2Diff = 100; p2Diff >= 0; p2Diff--){
 				ten = new Phase10(null);
 				ten.addPlayer(new AIPlayer(ten, p1Diff, "p1"));
 				ten.addPlayer(new AIPlayer(ten, p2Diff, "p2"));
-				ArrayList<Player> winners = ten.startGame();
+				ten.startGame();
+				ArrayList<Player> winners = ten.getWinners();
 				if(winners.get(0).getName().compareTo("p1") == 0)
 					System.out.print("1");
 				else
